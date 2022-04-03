b4ec2821f61ffc64c194f24cd19fcb012e89e8eb
ididntaskforthis <bw9999@gmail.com>
2022-04-02 21:11:26 -0500

    Add logging and use strong types for both sending and receiving from websocket.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::fs;
-use std::io::Write;
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
 use std::ptr::null_mut;
@@ -11,24 +9,53 @@ use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::um::processthreadsapi::ExitProcess;
 use winapi::um::timeapi::timeGetTime;
 use tungstenite::{stream::MaybeTlsStream, WebSocket, connect, Message};
-use serde_json::json;
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 use lazy_static::lazy_static;
 use std::sync::Mutex;
+use log::{debug, error, LevelFilter};
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
+
+static LOG_FILE_NAME: &str = "lamulanamw.log";
+static SERVER_URL: &str = "wss://la-mulana.arakiyda.com/cable";
 
 lazy_static! {
     static ref WEBSOCKET: Mutex<WebSocket<MaybeTlsStream<TcpStream>>> = {
-        let url = url::Url::parse("wss://la-mulana.arakiyda.com/cable").unwrap();
+        let url = url::Url::parse(SERVER_URL).unwrap();
         let (ws_connection, _) = connect(url).expect("Failed to connect");
         match ws_connection.get_ref() {
             MaybeTlsStream::NativeTls(ref tls) => {
-                tls.get_ref().set_nonblocking(true);
-                return;
+                tls.get_ref().set_nonblocking(true).map_err(|err| {
+                    error!("Could not set socket as nonblocking: {}", err);
+                }).unwrap();
             },
             _ => ()
         };
         return Mutex::new(ws_connection);
     };
+}
+
+#[derive(Serialize, Deserialize)]
+struct InitialPayload {
+    command: String,
+    identifier: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Identifier {
+    id: u64,
+    channel: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestMessagePayload {
+    identifier: String,
+    message: TestMessage
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestMessage {
+    body: String
 }
 
 unsafe extern "stdcall" fn init(patch_version: winapi::shared::ntdef::INT) {
@@ -38,35 +65,24 @@ unsafe extern "stdcall" fn init(patch_version: winapi::shared::ntdef::INT) {
         ExitProcess(1);
     }
 
-    let handshake_message = json!({
-        "command": "subscribe",
-        "identifier": json!({
-            "id": "15",
-            "channel": "MultiworldSyncChannel"
-        }).to_string()
-    }).to_string();
+    let ident = Identifier {
+        id: 15,
+        channel: "MultiworldSyncChannel".to_string()
+    };
+    let initial_payload = InitialPayload {
+        command: "subscribe".to_string(),
+        identifier: serde_json::to_string(&ident).unwrap()
+    };
 
-    WEBSOCKET.lock().unwrap().write_message(Message::text(handshake_message.to_string())).expect("Unable to Connect To Websocket Channel");
+    WEBSOCKET.lock().unwrap().write_message(Message::Text(serde_json::to_string(&initial_payload).unwrap())).expect("Unable to Connect To Websocket Channel");
 }
 
 unsafe extern "stdcall" fn game_loop() -> DWORD {
-    let mut file = fs::OpenOptions::new()
-        .create(true)
-        .write(true)
-        .append(true)
-        .open("websocket_messages.txt")
-        .unwrap();
-
-    WEBSOCKET.lock().unwrap().read_message().map(|message|{
+    let _ = WEBSOCKET.lock().unwrap().read_message().map(|message| {
         let data = message.into_data();
-        let json_data: Value = serde_json::from_slice(&data).unwrap();
-        match &json_data["message"]["body"] {
-            Value::String(body) => {
-                file.write_all(body.as_ref());
-                return;
-            },
-            _ => ()
-        }
+        let _ = serde_json::from_slice::<TestMessagePayload>(data.as_ref()).map(|payload| {
+            debug!("{:?}", payload);
+        });
     });
 
     return timeGetTime();
@@ -77,6 +93,15 @@ extern "stdcall" fn DllMain(_h_inst_dll: HINSTANCE, fdw_reason: DWORD, _lpv_rese
     if fdw_reason != DLL_PROCESS_ATTACH {
         return true;
     }
+    let file_appender = FileAppender::builder()
+        .build(LOG_FILE_NAME)
+        .unwrap();
+    let config = Config::builder()
+        .appender(Appender::builder().build("lamulanamw", Box::new(file_appender)))
+        .build(Root::builder().appender("lamulanamw").build(LevelFilter::Debug))
+        .unwrap();
+    log4rs::init_config(config).unwrap();
+
     unsafe {
         let rva0 = GetModuleHandleW(null_mut()).cast::<*const u8>().sub(0x100000);
         write_address(rva0, 0xdb9060, init as *const usize);
