9be0c12ffcd2f0c47c1d66aac5e60ede87636f24
ididntaskforthis <47310311+ididntaskforthis@users.noreply.github.com>
2022-08-28 02:06:54 -0500

    Refactor out application related code.

---
+++ src/application.rs
@@ -0,0 +1,63 @@
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::ptr::{null_mut};
+use winapi::um::winuser::{MB_OK, MessageBoxW};
+use log::debug;
+
+pub static OPTION_SDATA_NUM_ADDRESS: usize = 0x00db6fb7;
+pub static OPTION_SDATA_ADDRESS: usize = 0x00db7048;
+pub static OPTION_POS_CX_ADDRESS: usize = 0x00db7168;
+pub static OPTION_POS_CY_ADDRESS: usize = 0x00db714c;
+pub static SET_VIEW_EVENT_NS_ADDRESS: usize = 0x00507160;
+pub static ITEM_GET_AREA_INIT_ADDRESS: usize = 0x004b8950;
+
+pub struct Application {
+    pub address: *mut *const u8
+}
+
+impl Application {
+    pub unsafe fn write_address<T>(&self, offset: usize, f: T) {
+        self.get_address_from_offset(offset).cast::<T>().write(f);
+    }
+
+    pub unsafe fn write_address_with_additional_offset<T>(&self, offset: usize, additional_offset: usize, f: T) {
+        self.get_address_from_offset(offset).cast::<T>().add(additional_offset).write(f);
+    }
+
+    pub unsafe fn read_address<T>(&self, offset: usize) -> T {
+        self.get_address_from_offset(offset).cast::<T>().read()
+    }
+
+    pub unsafe fn get_address_from_offset(&self, offset: usize) -> *mut *const u8 {
+        let address = self.address.add(offset / 4);
+        debug!("Getting address of {:?} with requested offset of {}!", address, offset / 4);
+        address
+    }
+
+    pub unsafe fn option_stuck(&self, option_num: u32) {
+        let s_data_num: u8 = self.read_address(OPTION_SDATA_NUM_ADDRESS);
+        debug!("s_data_num is {}", s_data_num);
+        if s_data_num < 32 {
+            self.write_address_with_additional_offset(OPTION_SDATA_ADDRESS, s_data_num as usize, option_num);
+            self.write_address(OPTION_SDATA_NUM_ADDRESS, s_data_num + 1);
+        }
+    }
+
+    pub unsafe fn option_pos(&self, x: f32, y: f32) {
+        self.write_address(OPTION_POS_CX_ADDRESS, x);
+        self.write_address(OPTION_POS_CY_ADDRESS, y);
+    }
+
+    pub unsafe fn get_sdata_num(&self) -> u8 {
+        return self.read_address(OPTION_SDATA_NUM_ADDRESS);
+    }
+}
+
+fn create_wstring(str : &str) -> Vec<u16> {
+    return OsStr::new(str).encode_wide().chain(Some(0).into_iter()).collect();
+}
+
+pub unsafe fn show_message_box(message: &str) {
+    let converted_message = create_wstring(message).as_ptr();
+    MessageBoxW(null_mut(), converted_message, null_mut(), MB_OK);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-use std::ffi::OsStr;
-use std::os::windows::ffi::OsStrExt;
 use std::ptr::{null_mut};
 use std::net::TcpStream;
 use winapi::um::winnt::DLL_PROCESS_ATTACH;
-use winapi::um::winuser::{MB_OK, MessageBoxW};
 use winapi::shared::minwindef::*;
 use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::um::processthreadsapi::ExitProcess;
@@ -16,16 +13,12 @@ use log::{debug, error, LevelFilter};
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 
+pub mod application;
+use application::{ Application, show_message_box, SET_VIEW_EVENT_NS_ADDRESS, ITEM_GET_AREA_INIT_ADDRESS };
 static LOG_FILE_NAME: &str = "lamulanamw.log";
 static SERVER_URL: &str = "wss://la-mulana.arakiyda.com/cable";
 static mut GAME_SERVER_LOOP_COUNTER: u32 = 1;
-static mut APPLICATION_ADDRESS: *mut *const u8 = null_mut();
-static OPTION_SDATA_NUM_ADDRESS: usize = 0x00db6fb7;
-static OPTION_SDATA_ADDRESS: usize = 0x00db7048;
-static OPTION_POS_CX_ADDRESS: usize = 0x00db7168;
-static OPTION_POS_CY_ADDRESS: usize = 0x00db714c;
-static SET_VIEW_EVENT_NS_ADDRESS: usize = 0x00507160;
-static ITEM_GET_AREA_INIT_ADDRESS: usize = 0x004b8950;
+static mut APPLICATION: Option<Application> = None;
 
 lazy_static! {
     static ref WEBSOCKET: Mutex<WebSocket<MaybeTlsStream<TcpStream>>> = {
@@ -94,25 +87,34 @@ unsafe extern "stdcall" fn game_loop() -> DWORD {
         });
     });
 
-    debug!("Game loop counter is {}", GAME_SERVER_LOOP_COUNTER);
+    // debug!("Game loop counter is {}", GAME_SERVER_LOOP_COUNTER);
     if GAME_SERVER_LOOP_COUNTER % 2000 == 0 {
-        debug!("About to spawn item...");
-        option_pos(0.0, 0.0);
-        option_stuck(81);
-        option_stuck(32);
-        option_stuck(24);
-        option_stuck(39);
+        APPLICATION.as_ref().map(|app| {
+            app.option_pos(0.0, 0.0);
+            app.option_stuck(81);
+            app.option_stuck(32);
+            app.option_stuck(2);
+            app.option_stuck(39);
 
-        debug!("Executing setViewEventNs");
-        let set_view_event_ns = get_address_from_offset(SET_VIEW_EVENT_NS_ADDRESS).cast::<*const ()>();
-        let item_get_area_init = get_address_from_offset(ITEM_GET_AREA_INIT_ADDRESS);
-        let f: extern "C" fn(u16, usize) = std::mem::transmute(set_view_event_ns);
-        (f)(16, item_get_area_init as usize);
-        debug!("Finished executing setViewEventNs");
+            debug!("Executing setViewEventNs");
+            let set_view_event_ns = app.get_address_from_offset(SET_VIEW_EVENT_NS_ADDRESS).cast::<*const ()>();
+            let item_get_area_init = app.get_address_from_offset(ITEM_GET_AREA_INIT_ADDRESS);
+            let f: extern "C" fn(u16, *const usize) = std::mem::transmute(set_view_event_ns);
+            (f)(16, test_setns as *const usize);
+            debug!("Finished executing setViewEventNs");
+
+            let s_data_num = app.get_sdata_num();
+            debug!("s_data_num is now {}", s_data_num);
+        });
     }
     GAME_SERVER_LOOP_COUNTER = GAME_SERVER_LOOP_COUNTER + 1;
 
     return timeGetTime();
+}
+
+unsafe fn test_setns(ptr: usize) {
+    let init_message = format!("Success!");
+    show_message_box(&init_message);
 }
 
 #[no_mangle]
@@ -130,44 +132,15 @@ extern "stdcall" fn DllMain(_h_inst_dll: HINSTANCE, fdw_reason: DWORD, _lpv_rese
     log4rs::init_config(config).unwrap();
 
     unsafe {
-        APPLICATION_ADDRESS = GetModuleHandleW(null_mut()).cast::<*const u8>().sub(0x100000);
-        write_address(0xdb9060, init as *const usize);
-        write_address(0xdb9064, game_loop as *const usize);
+        let app = Application {
+            address: GetModuleHandleW(null_mut()).cast::<*const u8>().sub(0x100000)
+        };
+
+        app.write_address(0xdb9060, init as *const usize);
+        app.write_address(0xdb9064, game_loop as *const usize);
+
+        APPLICATION = Some(app);
         return true;
     }
 }
 
-unsafe fn write_address<T>(offset: usize, f: T) {
-    get_address_from_offset(offset).cast::<T>().write(f);
-}
-
-unsafe fn read_address<T>(offset: usize) -> T {
-    get_address_from_offset(offset).cast::<T>().read()
-}
-
-unsafe fn get_address_from_offset(offset: usize) -> *mut *const u8 {
-    APPLICATION_ADDRESS.add(offset/4)
-}
-
-unsafe fn show_message_box(message: &str) {
-    let converted_message = to_wstring(message).as_ptr();
-    MessageBoxW(null_mut(), converted_message, null_mut(), MB_OK);
-}
-
-unsafe fn option_stuck(option_num: u32) {
-    let s_data_num: u8 = read_address(OPTION_SDATA_NUM_ADDRESS);
-    if s_data_num & 0xe0 == 0 {
-        let array_offset = (s_data_num as u32 * 0x4) as usize;
-        write_address(OPTION_SDATA_ADDRESS + array_offset, option_num);
-        write_address(OPTION_SDATA_NUM_ADDRESS, s_data_num + 1);
-    }
-}
-
-unsafe fn option_pos(x: f32, y: f32) {
-    write_address(OPTION_POS_CX_ADDRESS, x);
-    write_address(OPTION_POS_CY_ADDRESS, y);
-}
-
-fn to_wstring(str : &str) -> Vec<u16> {
-    return OsStr::new(str).encode_wide().chain(Some(0).into_iter()).collect();
-}
