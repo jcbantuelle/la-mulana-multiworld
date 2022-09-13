60b51b29b9377ff7bcc667e53901e8968c139a47
jcbantuelle <jcbantuelle@gmail.com>
2022-09-13 09:29:27 -0500

    Fix Up Taskdata Memory Offsets

--- src/taskdata.rs
+++ src/taskdata.rs
@@ -70,17 +70,17 @@ impl std::fmt::Display for Clip {
 
 #[repr(C)]
 pub struct Image {
-    pub width: f32, // +4 = 4
-    pub height: f32, // +4 = 8
-    pub vcol: [VectorColor;4], // +[4*4] = 24
-    pub vc: u32, // +4 = 28
-    pub pc: u32, // +4 = 32
-    pub clip: Clip, // +16 = 48
-    pub tex_addr: u32, // +4 = 52
-    pub base_obj: [u32;6], // +[4*6] = 76
-    pub text_f: u32, // +4 = 80
-    pub rInvWidth: f32, // +4 = 84
-    pub rInvHeight: f32 // +4 = 88
+    pub width: f32,
+    pub height: f32,
+    pub vcol: [VectorColor;4],
+    pub vc: u32,
+    pub pc: u32,
+    pub clip: Clip,
+    pub tex_addr: u32,
+    pub base_obj: [u32;6],
+    pub text_f: u32,
+    pub rInvWidth: f32,
+    pub rInvHeight: f32
 }
 
 impl std::fmt::Display for Image {
@@ -114,14 +114,14 @@ impl std::fmt::Display for Image {
 
 #[repr(C)]
 pub struct TaskId {
-    pub no: i16, // +2 = 2
-    pub undefined1: u8, // +1 = 3
-    pub undefined2: u8, // +1 = 4
-    pub uid: u32, // +4 = 8
-    pub live: u8, // +1 = 9
-    pub undefined3: u8, // +1 = 10
-    pub undefined4: u8, // +1 = 11
-    pub undefined5: u8 // +1 = 12
+    pub no: i16,
+    pub undefined1: u8,
+    pub undefined2: u8,
+    pub uid: u32,
+    pub live: u8,
+    pub undefined3: u8,
+    pub undefined4: u8,
+    pub undefined5: u8
 }
 
 impl std::fmt::Display for TaskId {
@@ -181,70 +181,70 @@ impl std::fmt::Display for MyPro {
 
 #[repr(C)]
 pub struct TaskData {
-    pub init: Event, // +4 = 4
-    pub ffunc: EventWithBool, // +4 = 8
-    pub sfunc: Event, // +4 = 12
-    pub rfunc: EventWithBool, // +4 = 16
-    pub dfunc: Event, // +4 = 20
-    pub finalfunc: Event, // +4 = 24
-    pub hfunc: Event, // +4 = 28
-    pub sta: u32, // +4 = 32
-    pub sbuff: [i32;32], // +[4*32](128) = 160
-    pub addr: [*mut *const usize;16], // +[4*16](64) = 224
-    pub buff: [i32;32], // +[4*32](128) = 352
-    pub fbuff: [f32;32], // +[4*32](128) = 480
-    pub pos: Point3d, // +12 = 492
-    pub pos_bk: Point3d, // +12 = 504
-    pub base: Point3d, // +12 = 516
-    pub hp: i32, // +4 = 520
-    pub image: Image, // +88 = 608
-    pub anime: u16, // +2 = 610
-    pub ptrn: u16, // +2 = 612 <- This should be 612
-    pub draw_f: u16, // +2 = 614
-    pub undefined1: u8, // +1 = 615
-    pub undefined2: u8, // +1 = 616
-    pub sort_z: i32, // +4 = 620
-    pub st_flag_num: u8, // +1 = 621
-    pub undefined3: u8, // +1 = 622
-    pub undefined4: u8, // +1 = 623
-    pub undefined5: u8, // +1 = 624
-    pub st_flags: Void, // +4 = 628
-    pub stop_flag: u32, // +4 = 632
-    pub resetflag: u32, // +4 = 636
-    pub sysfunc: Event, // +4 = 640
-    pub farstfunc: EventWithBool, // +4 = 644
-    pub backfunc: EventWithBool, // +4 = 648
-    pub hit_si: f32, // +4 = 652
-    pub system_buff: [i32;17], // +[4*17](68) = 720
-    pub event_addr: Void, // +4 = 724
-    pub hit_data: i16, // +2 = 726
-    pub next: i16, // +2 = 728
-    pub back: i16, // +2 = 730
-    pub my_layer: i16, // +2 = 732
-    pub undefined6: u8, // +1 = 733
-    pub undefined7: u8, // +1 = 734
-    pub id: TaskId, // +12 = 746
-    pub nexttask: Event,
-    pub backtask: Event,
-    pub field_pri: u8,
-    pub room_pri: u8,
-    pub view_pri: u8,
-    pub field_no: u8,
-    pub room_no: u8,
-    pub view_no: u8,
-    pub undefined8: u8,
-    pub undefined9: u8,
-    pub cash_no: i32,
-    pub end_flag_num: u8,
-    pub undefined10: u8,
-    pub undefined11: u8,
-    pub undefined12: u8,
-    pub end_flags: Event,
-    pub my_draw_z: i32,
-    pub sub_draw_z: i32,
-    pub fx: f32,
-    pub fy: f32,
-    pub my_pro: MyPro
+    pub init: Event, // 0x0 (0) - 4 bytes
+    pub ffunc: EventWithBool, // 0x4 (4) - 4 bytes
+    pub sfunc: Event, // 0x8 (8) - 4 bytes
+    pub rfunc: EventWithBool, // 0xc (12) - 4 bytes
+    pub dfunc: Event, // 0x10 (16) - 4 bytes
+    pub finalfunc: Event, // 0x14 (20) - 4 bytes
+    pub hfunc: Event, // 0x18 (24) - 4 bytes
+    pub sta: u32, // 0x1C (28) - 4 bytes
+    pub sbuff: [i32;32], // 0x20 (32) - 128 bytes
+    pub addr: [*mut usize;16], // 0xA0 (160) - 64 bytes
+    pub buff: [i32;32], // 0xE0 (224) - 128 bytes
+    pub fbuff: [f32;32], // 0x160 (352) - 128 bytes
+    pub pos: Point3d, // 0x1E0 (480) - 12 bytes
+    pub pos_bk: Point3d, // 0x1EC (492) - 12 bytes
+    pub base: Point3d, // 0x1F8 (504) - 12 bytes
+    pub hp: i32, // 0x204 (516) - 4 bytes
+    pub image: Image, // 0x25C (604) - 88 bytes
+    pub anime: u16, // 0x25E (606) - 2 bytes
+    pub ptrn: u16, // 0x260 (608) - 2 bytes
+    pub draw_f: u16, // 0x262 (610) - 2 bytes
+    pub undefined1: u8, // 0x263 (611) - 1 byte
+    pub undefined2: u8, // 0x264 (612) - 1 byte
+    pub sort_z: i32, // 0x268 (616) - 4 bytes
+    pub st_flag_num: u8, // 0x269 (617) - 1 byte
+    pub undefined3: u8, // 0x26A (618) - 1 byte
+    pub undefined4: u8, // 0x26B (619) - 1 byte
+    pub undefined5: u8, // 0x26C (620) - 1 byte
+    pub st_flags: Void, // 0x270 (624) - 4 bytes
+    pub stop_flag: u32, // 0x274 (628) - 4 bytes
+    pub resetflag: u32, // 0x278 (632) - 4 bytes
+    pub sysfunc: Event, // 0x27C (636) - 4 bytes
+    pub farstfunc: EventWithBool, // 0x280 (640) - 4 bytes
+    pub backfunc: EventWithBool, // 0x284 (644) - 4 bytes
+    pub hit_si: f32, // 0x288 (648) - 4 bytes
+    pub system_buff: [i32;17], // 0x28C (652) - 68 bytes
+    pub event_addr: Void, // 0x2D0 (720) - 4 bytes
+    pub hit_data: i32, // 0x2D4 (724) - 4 bytes
+    pub next: i16, // 0x2D8 (728) - 2 bytes
+    pub back: i16, // 0x2DA (730) - 2 bytes
+    pub my_layer: i16, // 0x2DC (732) - 2 bytes
+    pub undefined6: u8, // 0x2DE (734) - 1 byte
+    pub undefined7: u8, // 0x2DF (735) - 1 byte
+    pub id: TaskId, // 0x2E0 (736) - 12 bytes
+    pub nexttask: Event, // 0x2EC (748) - 4 bytes
+    pub backtask: Event, // 0x2F0 (752) - 4 bytes
+    pub field_pri: u8, // 0x2F4 (756) - 1 byte
+    pub room_pri: u8, // 0x2F5 (757) - 1 byte
+    pub view_pri: u8, // 0x2F6 (758) - 1 byte
+    pub field_no: u8, // 0x2F7 (759) - 1 byte
+    pub room_no: u8, // 0x2F8 (760) - 1 byte
+    pub view_no: u8, // 0x2F9 (761) - 1 byte
+    pub undefined8: u8, // 0x2FA (762) - 1 byte
+    pub undefined9: u8, // 0x2FB (763) - 1 byte
+    pub cash_no: i32, // 0x2FC (764) - 4 bytes
+    pub end_flag_num: u8, // 0x300 (768) - 1 byte
+    pub undefined10: u8, // 0x301 (769) - 1 byte
+    pub undefined11: u8, // 0x302 (770) - 1 byte
+    pub undefined12: u8, // 0x303 (771) - 1 byte
+    pub end_flags: Event, // 0x304 (772) - 4 bytes
+    pub my_draw_z: i32, // 0x308 (776) - 4 bytes
+    pub sub_draw_z: i32, // 0x30C (780) - 4 bytes
+    pub fx: f32, // 0x310 (784) - 4 bytes
+    pub fy: f32, // 0x314 (788) - 4 bytes
+    pub my_pro: MyPro // 0x318 (792) - 28 bytes
 }
 
 impl std::fmt::Display for TaskData {
