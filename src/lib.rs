3bfb4bab20411b799e3a72384fa3c8ffd4397a64
jcbantuelle <jcbantuelle@gmail.com>
2022-09-24 21:48:50 -0500

    Decompose DLLMain

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use winapi::shared::minwindef::*;
 use serde::Deserialize;
 use log::LevelFilter;
 use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Config, load_config_file, Root};
+use log4rs::config::{Appender, Config, Root};
 use toml;
 use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::um::processthreadsapi::ExitProcess;
@@ -30,26 +30,35 @@ extern "stdcall" fn DllMain(_h_inst_dll: HINSTANCE, fdw_reason: DWORD, _lpv_rese
     }
 
     unsafe {
-        let _ = read_config_and_init_app().map_err(|err| {
+        let app_config = read_config().map_err(|err| {
             show_message_box(&err);
             ExitProcess(1);
-        });
+        }).unwrap();
+        init_logger(&app_config);
+        init_app(&app_config);
         return true;
     }
 }
 
-unsafe fn read_config_and_init_app() -> Result<(), String> {
+unsafe fn read_config() -> Result<AppConfig, String> {
     let file_contents = fs::read_to_string(CONFIG_FILENAME).map_err(|_| "Error reading config.".to_string())?;
     let app_config = toml::from_str::<AppConfig>(&file_contents).map_err(|_| "Error parsing config.".to_string())?;
+    Ok(app_config)
+}
+
+unsafe fn init_logger(app_config: &AppConfig) {
     let file_appender = FileAppender::builder()
-        .build(app_config.log_file_name)
+        .build(&app_config.log_file_name)
         .unwrap();
-    let config = Config::builder()
+    let log_config = Config::builder()
         .appender(Appender::builder().build("lamulanamw", Box::new(file_appender)))
         .build(Root::builder().appender("lamulanamw").build(LevelFilter::Debug))
         .unwrap();
-    log4rs::init_config(config).unwrap();
+    log4rs::init_config(log_config).unwrap();
+}
+
+unsafe fn init_app(app_config: &AppConfig) {
     let websocket = network::init(&app_config.server_url);
     let address = GetModuleHandleW(null_mut()).cast::<u8>().wrapping_sub(0x400000);
-    Ok(Application::attach(address, websocket))
+    Application::attach(address, websocket);
 }
