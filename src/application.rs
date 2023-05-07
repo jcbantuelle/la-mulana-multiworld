89b253d95439855a8dd4b74cdb6a442591b637e3
jcbantuelle <jcbantuelle@gmail.com>
2023-05-06 20:51:24 -0500

    Add Chest Flags

--- src/application.rs
+++ src/application.rs
@@ -39,6 +39,47 @@ pub static ITEM_SYMBOL_INIT_POINTER_ADDRESS: usize = 0x006d1174;
 pub static ITEM_SYMBOL_INIT_ADDRESS: usize = 0x004b8ae0;
 pub static ITEM_SYMBOL_BACK_ADDRESS: usize = 0x004b8e70;
 
+pub static ITEM_CHEST_FLAGS: [u32;38] = [
+    259, // Guidance, Treasures
+    171, // Guidance, Crucifix
+    169, // Guidance, Grail
+    229, // Guidance, yagostr.exe
+    182, // Surface, Feather
+    167, // Surface, Shell Horn
+    195, // Surface, Birth Seal (2)
+    170, // Temple of the Sun, Isis Pendant
+    174, // Temple of the Sun, Bronze Mirror
+    168, // Spring, Glove
+    177, // Spring, Scalesphere
+    194, // Spring, Origin Seal (1)
+    193, // Inferno, Ice Cape
+    196, // Extinction, Life Seal (3)
+    188, // Endless Corridor, Twin Statue
+    158, // Shrine of the Mother, Crystal Skull
+    260, // Shrine of the Mother, Diary
+    242, // Shrine of the Mother, bounce.exe
+    197, // Shrine of the Mother, Death Seal (4)
+    154, // Illusion, Cog of the Soul
+    162, // Illusion, Key of Eternity
+    184, // Illusion, Fairy Clothes
+    178, // Graveyard, Gauntlet
+    244, // Graveyard, mirai.exe
+    173, // Moonlight, Grapple Claw
+    187, // Moonlight, Fruit of Eden
+    163, // Moonlight, Serpent Staff
+    175, // Goddess, Eye of Truth
+    180, // Goddess, Plane Model
+    191, // Goddess, Spaulder
+    151, // Ruin, Djed Pillar
+    159, // Ruin, Vessel
+    190, // Birth, Perfume
+    161, // Birth, Woman Statue
+    192, // Birth, Dimensional Key
+    156, // Birth, Pochette Key
+    153, // Dimensional Corridor, Magatama Jewel
+    245, // Gate of Time Surface, lamulana.exe
+];
+
 lazy_static! {
     static ref ITEMS_TO_GIVE: Mutex<Vec<GivenItem>> = Mutex::new(vec![]);
     static ref PLAYER_ITEM: Mutex<Option<PlayerItem>> = Mutex::new(None);
