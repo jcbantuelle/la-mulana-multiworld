dc2a1ef1999bfa78e898a3be944924e057f543ae
ididntaskforthis <47310311+ididntaskforthis@users.noreply.github.com>
2024-12-28 19:46:20 -0600

    Grab process by child's ID instead of searching by process name

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -7,10 +7,11 @@ const LAMULANA_MW_DLL_NAME: &str = "LaMulanaMW.dll";
 fn main() {
     match process::Command::new(LAMULANA_EXECUTABLE_NAME).spawn() {
         Ok(mut p) => {
-            let target_process = OwnedProcess::find_first_by_name(LAMULANA_EXECUTABLE_NAME).unwrap();
+            let process_id = p.id();
+            let target_process = OwnedProcess::from_pid(process_id).unwrap();
             let syringe = Syringe::for_process(target_process);
 
-            println!("Injecting into process...");
+            println!("Injecting into {} of PID {} with {}.", LAMULANA_EXECUTABLE_NAME, process_id, LAMULANA_MW_DLL_NAME);
             match syringe.inject(LAMULANA_MW_DLL_NAME) {
                 Ok(_) => {
                     println!("Injected and now waiting on process exit.");
