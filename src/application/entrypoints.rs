19df8e11aa18e38d5e8238f4240695a4ff1376b4
jcbantuelle <jcbantuelle@gmail.com>
2025-04-15 09:16:02 -0500

    Update Egg Placements

--- src/application/entrypoints.rs
+++ src/application/entrypoints.rs
@@ -25,19 +25,20 @@ lazy_static! {
         ("11-1-0".to_string(), EggType::SingleEgg{egg_id: 171}),
         ("7-1-0".to_string(), EggType::SingleEgg{egg_id: 172}),
         ("6-9-0".to_string(), EggType::SingleEgg{egg_id: 173}),
-        ("3-0-1".to_string(), EggType::SingleEgg{egg_id: 174}),
+        ("3-0-1".to_string(), EggType::SingleEgg{egg_id: 208}),
         ("13-3-1".to_string(), EggType::SingleEgg{egg_id: 175}),
         ("5-9-0".to_string(), EggType::SingleEgg{egg_id: 176}),
         ("12-0-1".to_string(), EggType::SingleEgg{egg_id: 177}),
+        ("12-0-1".to_string(), EggType::MultiEgg{flag: 0x00c, egg_ids: vec![177,193]}),
         ("23-9-0".to_string(), EggType::SingleEgg{egg_id: 178}),
         ("11-3-0".to_string(), EggType::SingleEgg{egg_id: 179}),
         ("0-0-0".to_string(), EggType::SingleEgg{egg_id: 180}),
         ("2-4-0".to_string(), EggType::SingleEgg{egg_id: 175}),
-        ("4-2-0".to_string(), EggType::SingleEgg{egg_id: 181}),
+        ("4-2-0".to_string(), EggType::SingleEgg{egg_id: 207}),
         ("10-5-1".to_string(), EggType::SingleEgg{egg_id: 170}),
-        ("14-5-0".to_string(), EggType::SingleEgg{egg_id: 182}),
-        ("15-1-1".to_string(), EggType::SingleEgg{egg_id: 183}),
-        ("17-1-0".to_string(), EggType::SingleEgg{egg_id: 184}),
+        ("14-5-0".to_string(), EggType::SingleEgg{egg_id: 218}),
+        ("15-1-1".to_string(), EggType::SingleEgg{egg_id: 217}),
+        ("17-1-0".to_string(), EggType::SingleEgg{egg_id: 208}),
         ("8-4-3".to_string(), EggType::SingleEgg{egg_id: 184}),
         ("8-1-1".to_string(), EggType::SingleEgg{egg_id: 185}),
         ("9-0-0".to_string(), EggType::SingleEgg{egg_id: 186}),
@@ -47,15 +48,15 @@ lazy_static! {
         ("21-0-0".to_string(), EggType::MultiEgg{flag: 0x00c, egg_ids: vec![188,185]}),
         ("5-3-0".to_string(), EggType::SingleEgg{egg_id: 189}),
         ("2-1-1".to_string(), EggType::SingleEgg{egg_id: 186}),
-        ("12-4-3".to_string(), EggType::SingleEgg{egg_id: 171}),
-        ("8-5-2".to_string(), EggType::SingleEgg{egg_id: 190}),
+        ("12-4-3".to_string(), EggType::SingleEgg{egg_id: 213}),
+        ("8-5-2".to_string(), EggType::SingleEgg{egg_id: 199}),
         ("3-4-5".to_string(), EggType::SingleEgg{egg_id: 191}),
         ("1-6-1".to_string(), EggType::SingleEgg{egg_id: 192}),
         ("1-7-0".to_string(), EggType::SingleEgg{egg_id: 193}),
         ("2-2-0".to_string(), EggType::SingleEgg{egg_id: 190}),
-        ("3-7-1".to_string(), EggType::SingleEgg{egg_id: 174}),
+        ("3-7-1".to_string(), EggType::SingleEgg{egg_id: 212}),
         ("4-7-0".to_string(), EggType::SingleEgg{egg_id: 181}),
-        ("4-3-0".to_string(), EggType::SingleEgg{egg_id: 181}),
+        ("4-3-0".to_string(), EggType::SingleEgg{egg_id: 214}),
         ("5-6-0".to_string(), EggType::SingleEgg{egg_id: 194}),
         ("5-4-0".to_string(), EggType::SingleEgg{egg_id: 189}),
         ("6-7-1".to_string(), EggType::SingleEgg{egg_id: 195}),
@@ -63,11 +64,10 @@ lazy_static! {
         ("7-8-1".to_string(), EggType::SingleEgg{egg_id: 196}),
         ("9-2-1".to_string(), EggType::SingleEgg{egg_id: 197}),
         ("10-9-0".to_string(), EggType::SingleEgg{egg_id: 198}),
-        ("10-2-0".to_string(), EggType::SingleEgg{egg_id: 199}),
-        ("12-3-0".to_string(), EggType::SingleEgg{egg_id: 177}),
-        ("12-3-0".to_string(), EggType::SingleEgg{egg_id: 177}),
-        ("13-4-0".to_string(), EggType::SingleEgg{egg_id: 175}),
-        ("13-8-0".to_string(), EggType::SingleEgg{egg_id: 175}),
+        ("10-2-0".to_string(), EggType::SingleEgg{egg_id: 203}),
+        ("12-3-0".to_string(), EggType::SingleEgg{egg_id: 205}),
+        ("13-4-0".to_string(), EggType::SingleEgg{egg_id: 207}),
+        ("13-8-0".to_string(), EggType::SingleEgg{egg_id: 209}),
         ("12-9-1".to_string(), EggType::SingleEgg{egg_id: 177}),
         ("12-6-0".to_string(), EggType::SingleEgg{egg_id: 177}),
         ("14-1-1".to_string(), EggType::SingleEgg{egg_id: 182}),
@@ -104,7 +104,23 @@ lazy_static! {
         ("23-6-0".to_string(), EggType::SingleEgg{egg_id: 178}),
         ("23-22-0".to_string(), EggType::SingleEgg{egg_id: 178}),
         ("23-8-0".to_string(), EggType::SingleEgg{egg_id: 204}),
-        ("1-10-3".to_string(), EggType::SingleEgg{egg_id: 197})
+        ("1-10-3".to_string(), EggType::SingleEgg{egg_id: 197}),
+        ("6-3-0".to_string(), EggType::SingleEgg{egg_id: 206}),
+        ("1-7-1".to_string(), EggType::SingleEgg{egg_id: 210}),
+        ("6-0-0".to_string(), EggType::SingleEgg{egg_id: 211}),
+        ("23-14-1".to_string(), EggType::SingleEgg{egg_id: 204}),
+        ("3-1-0".to_string(), EggType::SingleEgg{egg_id: 174}),
+        ("3-3-0".to_string(), EggType::SingleEgg{egg_id: 190}),
+        ("10-6-0".to_string(), EggType::SingleEgg{egg_id: 215}),
+        ("2-8-2".to_string(), EggType::SingleEgg{egg_id: 216}),
+        ("16-3-1".to_string(), EggType::SingleEgg{egg_id: 183}),
+        ("5-0-1".to_string(), EggType::SingleEgg{egg_id: 205}),
+        ("5-3-2".to_string(), EggType::SingleEgg{egg_id: 190}),
+        ("13-5-0".to_string(), EggType::SingleEgg{egg_id: 206}),
+        ("13-0-1".to_string(), EggType::SingleEgg{egg_id: 209}),
+        ("0-4-1".to_string(), EggType::SingleEgg{egg_id: 215}),
+        ("14-2-1".to_string(), EggType::SingleEgg{egg_id: 182}),
+        ("7-15-1".to_string(), EggType::SingleEgg{egg_id: 182})
     ]);
 }
 
@@ -121,13 +137,14 @@ pub fn game_loop() {
         let inventory_descriptions_card = unsafe { (*script_header.add(2)).data as *mut ScriptSubHeader};
         let waterproof_case_line = unsafe { &mut *inventory_descriptions_card.add(36) };
 
-        let eggs_found_text = format!("Easter Egg：(Find as many as you can!) You've found {egg_count} so far");
+        let eggs_found_text = format!("Easter Egg：You've found {egg_count} so far");
+        let space_count = eggs_found_text.chars().filter(|c| *c == ' ').count();
         let mut encoded_eggs_found_text = screenplay::encode(eggs_found_text);
         encoded_eggs_found_text.push(0x000a);
         *waterproof_case_line = ScriptSubHeader {
             pointer: encoded_eggs_found_text.as_ptr() as usize,
             data_num: encoded_eggs_found_text.len() as i32,
-            font_num: (encoded_eggs_found_text.len() - 12) as i32
+            font_num: (encoded_eggs_found_text.len() - space_count - 1) as i32
         };
     }
     application.original_game_loop()
